c143323bea00e22e58e197a757a02fe14e93c5c1
Guru Prakash Davala <guru.govindaraj@gmail.com>
2021-04-25 21:19:55 +0100

    Added comments

--- server.js
+++ server.js
@@ -11,32 +11,18 @@ const con = mysql.createConnection({
   database: "bookmein",
 });
 
-// con.connect(function (err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-//   con.query("SELECT * FROM attendees", function (err, result, fields) {
-//     if (err) throw err;
-//     console.log(result);
-//   });
-// });
-
 //User Body-Parser to access request body
-//parse application/json
 app.use(bodyParser.json());
 
 //CORS Configuration
 app.use(cors());
 
+//Default route - localhost 3000
 app.get("/", (req, res) => {
-  // con.query("SELECT * FROM attendees", function (err, result, fields) {
-  //   if (err) throw err;
-  //   res.send(result);
-  // });
-  res.send("Your API is working fine");
+  res.send("Your API is working fine today");
 });
 
-//popular sessions based on registrations
-
+//API for popular sessions based on registrations
 app.get("/eventregistrations", (req, res) => {
   con.query(
     "SELECT registrations.event_id, COUNT(registrations.event_id) as total_registrations, events.name FROM bookmein.registrations INNER JOIN bookmein.events ON registrations.event_id = events.id WHERE event_id IN (SELECT id FROM bookmein.events WHERE event_type = '86') GROUP BY event_id ORDER BY total_registrations DESC;",
@@ -47,7 +33,7 @@ app.get("/eventregistrations", (req, res) => {
   );
 });
 
-//popular sessions based on total time spent
+//API for popular sessions based on total time spent
 app.get("/geteventsbytotaltimespent", (req, res) => {
   con.query(
     "SELECT attendee_session_tracking.eventid, COUNT(attendee_session_tracking.eventid) as totaltime, events.name FROM bookmein.attendee_session_tracking  INNER JOIN bookmein.events ON attendee_session_tracking.eventid = events.id GROUP BY eventid  ORDER BY totaltime DESC;",
@@ -58,14 +44,7 @@ app.get("/geteventsbytotaltimespent", (req, res) => {
   );
 });
 
-app.get("/getallattendeesmessages", (req, res) => {
-  console.log("entered route");
-  con.query("SELECT * from attendees;", function (err, result, fields) {
-    if (err) throw err;
-    res.send(result);
-  });
-});
-
+//API to get all attendee-attendee conversations - based on the route param
 //Get All messages by attendeeID
 app.get("/getallattendeesmessages/:attendeeID", (req, res) => {
   //console.log("entered route");
@@ -80,7 +59,8 @@ app.get("/getallattendeesmessages/:attendeeID", (req, res) => {
   );
 });
 
-//Get all conversation between delegates and exhibior
+//API to get all conversation between attendee and exhibior - based on the route param
+//Get All messages by attendeeID
 app.get("/getallexhibitorsanddelegatesconversation/:attendeeID", (req, res) => {
   con.query(
     "SELECT * FROM bookmein.exhibitor_conversation INNER JOIN bookmein.exhibitor_message ON exhibitor_conversation.id = exhibitor_message.conversationid WHERE delegateid = " +
@@ -93,7 +73,7 @@ app.get("/getallexhibitorsanddelegatesconversation/:attendeeID", (req, res) => {
   );
 });
 
-//Get attendee details
+//API to get single attendee detail based on the route param
 app.get("/getattendeedetails/:attendeeID", (req, res) => {
   con.query(
     "SELECT id, first_name, last_name FROM bookmein.attendees where id = " +
@@ -106,6 +86,7 @@ app.get("/getattendeedetails/:attendeeID", (req, res) => {
   );
 });
 
+//API to fetch all events attended by attendee - based on the route param
 app.get("/getalleventsattendedbyattendee/:attendeeID", (req, res) => {
   con.query(
     "SELECT attendeeid,eventid, COUNT(attendeeid)/6 as totaltimespent from bookmein.attendee_session_tracking WHERE attendeeid = " +
@@ -118,18 +99,7 @@ app.get("/getalleventsattendedbyattendee/:attendeeID", (req, res) => {
   );
 });
 
-app.get("/totalevent", (req, res) => {
-  con.query(
-    "SELECT eventid from bookmein.attendee_session_tracking where eventid=3457;",
-    function (err, result, fields) {
-      if (err) throw err;
-      res.send(result);
-      console.log(result.length);
-    }
-  );
-});
-
-//Total registered
+//API to get total attendees registered
 app.get("/totalregistered", (req, res) => {
   con.query(
     "SELECT count(attendee_id) as totalregistrations FROM bookmein.registrations;",
@@ -140,7 +110,7 @@ app.get("/totalregistered", (req, res) => {
   );
 });
 
-//Total Attended
+//API to get total attendees attended
 app.get("/totalattended", (req, res) => {
   con.query(
     "SELECT count(distinct attendeeid) as totalattended from bookmein.attendee_session_tracking;",
@@ -151,7 +121,7 @@ app.get("/totalattended", (req, res) => {
   );
 });
 
-//Total Attended grouped by event_id
+//API to get Total Attended grouped by event_id
 app.get("/attendeesgroupedbyeventid", (req, res) => {
   con.query(
     "SELECT count(distinctAttendees.attendeeid) AS totalAttended, distinctAttendees.eventid, events.name FROM (SELECT DISTINCT attendeeid, eventid FROM bookmein.attendee_session_tracking) distinctAttendees INNER JOIN bookmein.events ON distinctAttendees.eventid = events.id GROUP BY distinctAttendees.eventid ORDER BY totalAttended DESC;",
@@ -162,7 +132,7 @@ app.get("/attendeesgroupedbyeventid", (req, res) => {
   );
 });
 
-//Stands visited and duration
+//APi to get total stands visited and duration spent
 app.get("/standsvisitedandduration", (req, res) => {
   con.query(
     "SELECT eventid, COUNT(eventid) AS totaltime, events.name FROM bookmein.stand_attendance INNER JOIN bookmein.events ON stand_attendance.eventid = events.id WHERE eventid IN (SELECT id from bookmein.events where event_type = 87) GROUP BY eventid;",
